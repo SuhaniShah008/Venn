39e77f3e13393bcc771a590cf78a646897eb7a61
Suhani Shah <fashionvbc@gmail.com>
2025-05-05 15:46:34 -0500

    Add files via upload

---
+++ Venn.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Venn{
+
+	private static String bat= "hello there friends";
+	private static String cat= "hi there calm";
+	private static String oneWord;
+
+
+
+
+
+	public static void main(String[] args){
+
+		Set<String> uniques = new HashSet<String>();
+        Set<String> dups    = new HashSet<String>();
+      
+       	oneWord=  bat+cat;
+       	for(String firstRecord : uniques){
+       		uniques.add(oneWord);
+            // comparing logic
+        	}
+    	}
+            
+
+
+        System.out.println("Duplicate words: " + dups);
+
+
+    }
+
+}
